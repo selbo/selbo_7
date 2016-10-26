8dcf58c9e653bb2c8b5cfcdaa40fc993f11c3d75
livneshai <shai.livneh@intel.com>
2016-10-26 17:53:09 +0300

    operating leds on socket
    operating leds on socket

--- main.js
+++ main.js
@@ -1,34 +1,94 @@
 var mraa = require("mraa"); //require mraa
 var WebSocket = require('ws');
 
-var ws = new WebSocket('ws://echo.websocket.org');
-//var ws = new WebSocket('ws://104.198.196.211:8000');
-ws.on('open', function open() {
-    var jsonString='{"cmd": "led2", "data": 1 }';
-        ws.send(jsonString);
+//var ws = new WebSocket('ws://echo.websocket.org');
+var ws = null;
+var connect_interval=3000; 
+var ON_OFF = 0.0 ;
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
+
+function send(command,data_)
+{
+    if (ws==null)
+        return;
+    if (ws.readyState!=1)//we are connected 
+        {
+            console.log("Cant send ...s");
+            return;
+        }
+    var d={};
+    d.cmd=command;
+    d.data=data_;
+    ws.send(JSON.stringify(d));
+}
+
+var restart_limit=5; 
+var restart_counter=restart_limit;
+function connect(){
+   // console.log("connection hard beat");
     
-});
+   // console.log("connection attempt:%s",ws.readyState);
+    if (ws!=null)
+    {
+        console.log("connection attempt:%s",ws.readyState);
+        if (ws.readyState==1)//we are connected 
+        {
+            console.log("Connection success");
+            restart_counter=restart_limit;
+            return;
+        }
+        if (ws.readyState==0)//we are connected 
+        {
+            restart_counter-=1;
+            console.log("prepare to reconnect");
+            setTimeout(connect,3000);
+            console.log("get out reconnect");
+            if (restart_counter!=0)
+                {
+                     return;
+                    
+                }
+           restart_counter=restart_limit;
+        }
+    }
+    ws = new WebSocket('ws://104.198.196.211:8000');
+    ws.on('open', function open() {
+        /*if (ON_OFF == 0.0){
+            console.log("send led is on");
+            var ledStringon='{"cmd": "led_status", "status": "on"}';
+            ws.send(ledStringon);
+        }
+        else{
+            console.log("send led is off");
+            var ledStringooff='{"cmd": "led_status", "status": "off"}';
+            ws.send(ledStringoff);
+        }*/
+        
+    //var jsonString='{"cmd": "led", "data": "on"}';
+     //   ws.send(jsonString);
+      console.log("Connection open");
+    });
+ //   console.log("Connection after open");
+    ws.on('close', function open() {
+        console.log("connection closed:%s",ws.readyState);
+        connect()
+
+     });
+    console.log("Connection before on msg");
+    ws.on('message',on_message); 
+ //   console.log("Connection after message"); 
+    
+    
+    setTimeout(connect,connect_interval);
+//    console.log("Connection after connect back");
+}
 
 
-
-// var mraa = require("mraa"); //require mraa
-
-var pwm3 = new mraa.Pwm(3); 
-pwm3.enable(true);
-pwm3.period_us(2000);
-pwm3.write(0);
-
-var pwm5 = new mraa.Pwm(5); 
-pwm5.enable(true);
-pwm5.period_us(2000);
-pwm5.write(0.0); 
-
-var pwm6 = new mraa.Pwm(6); 
-pwm6.enable(true);
-pwm6.period_us(2000);
-pwm6.write(0.0);
-
-ws.on('message', function(data, flags) {
+function on_message(data, flags) {
+// console.log("inside on_msg");  
     console.log("received:"+data);
     var json=null;
     try {
@@ -42,51 +102,135 @@ ws.on('message', function(data, flags) {
 	
 	//json.cmd //led
 	//json.data //on/off
-    
 
-   
-    if (json.cmd == "led1") {
+   console.log("Connection after msg recv");
+  if (json.cmd == "led"){
+    if (json.data == "on") {
+            ON_OFF = 1.0;
+       //     send("led_status","off");
+            console.log("send led is closed");
+        }
+        else{
+            ON_OFF = 0.0;
+      //      send("led_status","on");
+            console.log("send led is closed");
+        }
+  }
+    
+    if (json.cmd == "led") {
+        /*
+        if (json.data == "on") {
+            ON_OFF = 1.0;
+            send("led_status","on")
+        }
+        else{
+            ON_OFF = 0.0;
+            send("led_status","off")
+        }
+       */ 
       pwm3.enable(true);
       pwm3.period_us(2000);
-      pwm3.write(json.data); 
+      pwm3.write(1.0); 
     }
-    else{
-      pwm3.enable(true);
-      pwm3.period_us(2000);
-      pwm3.write(0.0);
-    }
+//    else{
+//      pwm3.enable(true);
+//      pwm3.period_us(2000);
+//      pwm3.write(0.0);
+//    }
     
     
-   if (json.cmd == "led2") {
+   if (json.cmd == "led") {
+       /*
+       if (json.data == "on") {
+            ON_OFF = 1.0;
+        }
+        else{
+            ON_OFF = 0.0;
+        }
+       */
       pwm5.enable(true);
       pwm5.period_us(2000);
-      pwm5.write(json.data); 
+      pwm5.write(ON_OFF); 
     }
-    else{
-      pwm5.enable(false);
-      pwm5.period_us(2000);
-      pwm5.write(0.0);
-    } 
+ //   else{
+ //     pwm5.enable(false);
+ //     pwm5.period_us(2000);
+ //     pwm5.write(0.0);
+//    } 
     
     
-  if (json.cmd == "led3") {
+  if (json.cmd == "led") {
+      /*
+      if (json.data == "on") {
+            ON_OFF = 1.0;
+        }
+        else{
+            ON_OFF = 0.0;
+        }
+      */
       pwm6.enable(true);
       pwm6.period_us(2000);
-      pwm6.write(json.data); 
+      pwm6.write(ON_OFF); 
     }
-    else{
-      pwm6.enable(false);
-      pwm6.period_us(2000);
-      pwm6.write(0.0);
+ //   else{
+  //    pwm6.enable(false);
+ //     pwm6.period_us(2000);
+ //     pwm6.write(0.0);
+ //   }
+
+    
+    if (json.cmd == "led") {
+        if (json.data == "on") {
+           // ON_OFF = 1.0;
+            console.log("send led is closed");
+        //    send("led_status","off");
+            setTimeout(send("led_status","on"),10000);
+            
+        }
+        else{
+            //ON_OFF = 0.0;
+            console.log("send led is closed");
+          //  send("led_status","on");
+            setTimeout(send("led_status","off"),10000);
+        }
     }
+}
+
+
+
+
+
+// var mraa = require("mraa"); //require mraa
+
+var pwm3 = new mraa.Pwm(3); 
+pwm3.enable(true);
+pwm3.period_us(2000);
+pwm3.write(1.0);
+
+var pwm5 = new mraa.Pwm(5); 
+pwm5.enable(true);
+pwm5.period_us(2000);
+pwm5.write(0.0); 
+
+var pwm6 = new mraa.Pwm(6); 
+pwm6.enable(true);
+pwm6.period_us(2000);
+pwm6.write(0.0);
+
+
+connect();
+
+// console.log("Connection before msg recv");
+
+
 
 
     
-    console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
 
  
     
-    
+  /*  
 var myDigitalPin8 = new mraa.Gpio(8); //setup digital read on Digital pin #6 (D6)
 myDigitalPin8.dir(mraa.DIR_IN); //set the gpio direction to input
 
@@ -118,4 +262,4 @@ console.log(analogValue1); //write the value of the analog pin to the console
     //data.value = ledState;
     //io.emit('toogle led', data);
     //ledState = !ledState; //invert the ledState
-});
+*/
